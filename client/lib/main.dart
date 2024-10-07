887b0914cce210fd72bd38872dbefed8ff9cc5f1
jose frometa <josefrometa@holistika.co>
2024-10-07 16:41:32 -0400

    update user persistance logic

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -9,21 +10,16 @@ import 'package:starter_architecture_flutter_firebase/src/app.dart';
 import 'package:starter_architecture_flutter_firebase/src/features/authentication/data/firebase_auth_repository.dart';
 import 'package:starter_architecture_flutter_firebase/src/features/prompt/presentation/widgets/image_input_widget.dart';
 import 'package:starter_architecture_flutter_firebase/src/localization/string_hardcoded.dart';
-// ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:starter_architecture_flutter_firebase/src/util/device_info.dart';
-// import 'package:firebase_vertexai/firebase_vertexai.dart';
 import 'package:localstorage/localstorage.dart';
 
 late final ValueNotifier<int> notifier;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // turn off the # in the URLs on the web
   usePathUrlStrategy();
-  // * Register error handlers. For more info, see:
-  // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
-  // * Initialize Firebase
+
   await initLocalStorage();
   notifier = ValueNotifier(int.parse(localStorage.getItem('counter') ?? '0'));
   notifier.addListener(() {
@@ -32,11 +28,16 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // Create a ProviderContainer to read providers outside of the widget tree
   final container = ProviderContainer();
-  await container.read(authRepositoryProvider).signInAnonymously();
 
-  // * Entry point of the app
+  // Check if the user is already signed in
+  final authRepo = container.read(authRepositoryProvider);
+  final currentUser = FirebaseAuth.instance.currentUser;
+
+  // Sign in anonymously if no user is signed in
+  if (currentUser == null) {
+    await authRepo.signInAnonymously();
+  }
 
   deviceInfo = await DeviceInfo.initialize(DeviceInfoPlugin());
   if (DeviceInfo.isPhysicalDeviceWithCamera(deviceInfo)) {
@@ -53,17 +54,16 @@ Future<void> main() async {
 }
 
 void registerErrorHandlers() {
-  // * Show some error UI if any uncaught exception happens
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.presentError(details);
     debugPrint(details.toString());
   };
-  // * Handle errors from the underlying platform/OS
+
   PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
     debugPrint(error.toString());
     return true;
   };
-  // * Show some error UI when any widget in the app fails to build
+
   ErrorWidget.builder = (FlutterErrorDetails details) {
     return Scaffold(
       appBar: AppBar(
